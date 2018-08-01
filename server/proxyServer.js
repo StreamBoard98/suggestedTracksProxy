7713e3a2d93b1be73a484534c84312687456f1f8
Kristie Desiree <kly5@mail.sfsu.edu>
2018-08-01 11:47:19 -0700

    Make proxy server serve the static page based on an input id

--- server/proxyServer.js
+++ server/proxyServer.js
@@ -1,28 +1,16 @@
-// var http = require('http'),
-//     httpProxy = require('http-proxy');
-// //
-// // Create your proxy server and set the target in the options.
-// //
-// httpProxy.createProxyServer({target:'http://localhost:9000'}).listen(4001); // See (†)
- 
-// //
-// // Create your target server
-// //
-// http.createServer(function (req, res) {
-//   res.writeHead(200, { 'Content-Type': 'text/plain' });
-//   res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
-//   res.end();
-// }).listen(9000);
-
 const express = require('express');
 const path = require('path');
+
 const morgan = require('morgan');
 const app = express();
+
+app.use(morgan('dev'));
+app.use('/songs/:id', express.static(path.join(__dirname, '../public')));
+// app.use(express.static(path.join(__dirname, '../public')));
 
 
 app.listen(2000, () => console.log('Proxy server conected on 2000'));
 
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(morgan('dev'));
-
-console.log(path.join(__dirname, '../client/dist'));
+/*when the proxy receives a get request from /songs/:id
+it triggers a get request in the each of the four components
+*/
